7b8ea68aa2c7c5fb1f7444a1c982c21f1a07f28a
Farids Marchdiansyah <104078732+Rids300@users.noreply.github.com>
2023-08-11 23:16:50 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,37 +1,24 @@
-<div class="login-box">
-    <div class="login-logo text-center">
-        <!-- <img src="<?= base_url('assets/dist/img/logo.png') ?>" alt="Logo Sanjaya" style="width: 200px;"> -->
-        <a href=""><b>Admin</b> Login</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-        <?= $this->session->flashdata('message'); ?>
-        <form action="" method="post">
-            <div class="form-group has-feedback">
-                <input type="text" class="form-control" placeholder="Username" name="username" required>
-                <span class="glyphicon glyphicon-user form-control-feedback"></span>
-            </div>
-            <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Password" name="password" required>
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-            </div>
-            <div class="row">
-              <div class="col-xs-8">
-                <div class="checkbox icheck">
-                  <label class="">
-                    <div class="icheckbox_square-blue" aria-checked="false" aria-disabled="false" style="position: relative;"><input type="checkbox" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> Remember Me
-                  </label>
-                </div>
-              </div>
-              <!-- /.col -->
-              <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-              </div>
-              <!-- /.col -->
-            </div>
-        </form>
 
+<div class="row" style="margin-top:50px">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+        <h3>Login Page</h3>
+        <?= $this->session->flashdata('message'); ?>
+        <div class="box">
+            <div class="box-body">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="no_reg">No Registrasi</label>
+                        <input type="text" name="no_reg" id="no_reg" min="0" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" min="0" class="form-control" required>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary pull-right btn-flat">Sign In</button>
+                </form>
+            </div>
+        </div>
     </div>
-    <!-- /.login-box-body -->
+    <div class="col-md-4"></div>
 </div>
